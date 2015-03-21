7f1f66a9badb2487ccd1dc90fd2ff58b6b3dd74f
Hay-Z <hberge17@gmail.com>
2015-03-21 11:36:48 -0600

    Update
    Update

--- routes/noteView.js
+++ routes/noteView.js
@@ -1,8 +1,10 @@
 module.exports = function(app) {
 
     app.get('/note/view/:id', function(req, res) {
+
     	var id = req.params.id
-        var item = app.data.bully[id]
+    	var y = id-1
+        var item = app.data.bully[y]
 
         res.render('noteView.jade', {
             item: item
